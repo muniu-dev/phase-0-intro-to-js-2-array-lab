4c1477e7a8a1ed77006345749d4c059133fca921
Michael <muniu.dev@gmail.com>
2023-10-18 10:49:17 +0300

    lab completed passing 8/9

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+     return cats;
+ }
+
+ destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+destructivelyPrependCat('bob');
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    const appendCat = [...cats, `${name}`]
+    return appendCat;
+}
+appendCat("Broom")
+
+function prependCat(name){
+    const prependCat = [`${name}`, ...cats]
+    return prependCat;
+}
+prependCat("Arnold")
+
+function removeLastCat(){
+   const removeLastCat = cats.slice(0,-1)
+    return removeLastCat;
+}
+
+function removeFirstCat(){
+    const rvd = cats.slice(1);
+    return rvd;
+ }
+
